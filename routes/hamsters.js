2b7ba7c2f72fe9bef0eff934c805884bc3ac1f93
Leon Wass Vallin <leon.wass.vallin@mail.com>
2021-04-21 14:40:20 +0200

    Hamster route done maybe some more error handling left.

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -55,4 +55,83 @@ router.get("/:id", async (req, res) => {
   res.send(data);
 });
 
+router.post("/", async (req, res) => {
+  const obj = req.body;
+
+  if (!checkHamsterObj(obj)) {
+    res.status(400).send("Wrong object structure.");
+    return;
+  }
+
+  await database.collection("hamster").add(obj);
+  res.send("Hamster added.");
+});
+
+router.put("/:id", async (req, res) => {
+  const obj = req.body;
+  const id = req.params.id;
+
+  const hamsterRef = database.collection("hamster");
+  const snapshot = await hamsterRef.get();
+
+  let hamsterId = false;
+  snapshot.forEach((doc) => {
+    if (id === doc.id) {
+      hamsterId = true;
+    }
+  });
+
+  // Kolla med david 400 bad request error handling.
+  // Kolla med david om det behövs mer felhantering.
+  if (!obj) {
+    res.status(400).send("Wrong object structure.");
+    return;
+  }
+
+  if (!hamsterId) {
+    res.status(404).send("There is no hamster with that id.");
+    return;
+  } else {
+    await database.collection("hamster").doc(id).set(obj, { merge: true });
+    res.send("Hamster changed.");
+  }
+});
+
+// Kolla med david 400 bad request error handling.
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const hamsterRef = database.collection("hamster");
+  const snapshot = await hamsterRef.get();
+
+  let hamsterId = false;
+  snapshot.forEach((doc) => {
+    if (id === doc.id) {
+      hamsterId = true;
+    }
+  });
+
+  if (!hamsterId) {
+    res.status(404).send("There is no hamster with that id.");
+    return;
+  } else {
+    await database.collection("hamster").doc(id).delete();
+    res.send("Hamster deleted.");
+  }
+});
+
+function checkHamsterObj(obj) {
+  if (
+    obj.hasOwnProperty("name") &&
+    obj.hasOwnProperty("age") &&
+    obj.hasOwnProperty("favFood") &&
+    obj.hasOwnProperty("loves") &&
+    obj.hasOwnProperty("imgName") &&
+    obj.hasOwnProperty("wins") &&
+    obj.hasOwnProperty("defeats") &&
+    obj.hasOwnProperty("games")
+  )
+    return true;
+  else return false;
+}
+
 module.exports = router;
