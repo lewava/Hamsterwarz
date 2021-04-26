847e082a096734c0c518aa4e8cbd126396851d7d
Leon Wass Vallin <leon.wass.vallin@mail.com>
2021-04-26 10:39:25 +0200

    Added static folders

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const hamsters = require("./routes/hamsters.js");
+const path = require("path");
 
 const PORT = 2000;
+const publicFolder = path.join(__dirname, "public");
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`, req.params);
@@ -12,6 +14,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(publicFolder));
 
 app.use("/hamsters", hamsters);
 
